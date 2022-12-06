d77812585d88b5db06de4a5ad975b6c34e72d3ad
agrich-8 <a8grich@gmail.com>
2022-12-06 23:15:18 +0300

    tests

--- API/app/config.py
+++ API/app/config.py
@@ -3,7 +3,7 @@ import os
 from pydantic import BaseSettings
 
 
-file_path = os.path.abspath(os.getcwd())+r"\API\app\db\database.db"
+# file_path = os.path.abspath(os.getcwd())+r"\API\app\db\database.db"
 
 SECRET_KEY = 'CsJWl1OLyjkw8RJHEvAHoAtpnlbF/+fYd02D+lpAoOE='
 ALGORITHM = "HS256"
@@ -13,11 +13,10 @@ class Settings(BaseSettings):
 
     DB_USER = "root"
     DB_PASSWORD = "88uUheEWfk3"
-    DB_HOST = "mysql-db"
-    DB_PORT = "3306"
+    DB_HOST = "localhost"
     DATABASE = "tmdb"
-    SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:88uUheEWfk3@localhost/tmdb' #"sqlite:///"+file_path
-    # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://{}:{}@{}/{}?port={}?charset=utf8'.format(DB_USER, DB_PASSWORD, DB_HOST, DATABASE, DB_PORT)
+
+    SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DATABASE}' #"sqlite:///"+file_path
     server_host: str = '127.0.0.1'
     server_port: int = 8003
 
--- API/app/tests.py
+++ API/app/tests.py
@@ -1,44 +1,184 @@
+import random
 from fastapi.testclient import TestClient
 
 from .main import app
 
 client = TestClient(app)
 
-TEST_NAME = 'USER_TESTNAME'
-TEST_EMAIL = 'USER_TESTEMAIL'
-TEST_PASSWORD = 'USER_TESTPASSWORD'
+TEST_NAME = 'USER_TEST_NAME543v1111433'
+TEST_EMAIL = 'usertestemail.ex111s4123@gmail.com'
+TEST_PASSWORD = 'USER_TEST_PASSWORD'
+
+token = ''
+test_project_id = int()
+test_task_id = int()
 
 
-def test_create_item():
+
+def test_create_user():
+
     response = client.post(
-        "/user",
-        headers={"X-Token": "coneofsilence"},
-        json={"username": TEST_NAME, "email": TEST_EMAIL, "password": TEST_PASSWORD},
-    )
+                        "/user",
+                        json={"username": TEST_NAME, "email": TEST_EMAIL, "password": TEST_PASSWORD},
+                        )
     assert response.status_code == 200
     json = response.json()
     for key, value in json.items():
-        assert key in ['username', 'email', 'id', 'projects']
+        assert key in [
+                        'username', 
+                        'email', 
+                        'id', 
+                        'projects'
+                        ]
 
 
 def test_create_token():
+    global token
     response = client.post(
-                            "/main/token/",
-                            data={"username": "string", "title": "string"}
-                            )
+                        "/auth/token",
+                        data={"username": TEST_NAME, "password": TEST_PASSWORD}
+                        )
     assert response.status_code == 200
-    assert response.json() == {
-        "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHJpbmciLCJleHAiOjE2Njg4OTk2MzB9.U5NRU4e4LP19VoJyn1VBIwnp8_LTiv-819r0x8ymuuA",
-        "token_type": "bearer"
-        }
+    json =  response.json()
+    for key, value in json.items():
+        assert key in ['access_token', 'token_type']
+    token = json['access_token']
 
 
-def test_read_item():
-    response = client.get("/user", headers={"X-Token": "coneofsilence"})
+def test_get_user():
+    response = client.get(
+                        "/user", 
+                        headers={"Authorization": "Bearer "+token}
+                        )
     assert response.status_code == 200
-    assert response.json() == {
-        "id": "foo",
-        "title": "Foo",
-        "description": "There goes my hero",
-    }
+    json = response.json()
+    for key, value in json.items():
+        assert key in [
+                        'username', 
+                        'email', 
+                        'id', 
+                        'projects', 
+                        'tasks'
+                        ]
 
+
+def test_create_project():
+    global test_project_id
+    response = client.post(
+                        "/project",
+                        headers={"Authorization": "Bearer "+token},
+                        json={
+                            "title": "string",
+                            "color": "string"
+                            }
+                        )
+    assert response.status_code == 200
+    json = response.json()
+    for key, value in json.items():
+        assert key in [
+                        'id', 
+                        'title', 
+                        'color', 
+                        'is_base_project', 
+                        'user_id', 
+                        'tasks'
+                        ]
+    test_project_id = json['id']
+
+
+def test_update_project():
+    response = client.put(
+                        "/project",
+                        headers={"Authorization": "Bearer "+token},
+                        json={
+                            "id": test_project_id,
+                            "title": "string_new",
+                            "color": "string_new"
+                            }
+                        )
+    assert response.status_code == 200
+    json = response.json()
+    for key, value in json.items():
+        assert key in [
+                        'id', 
+                        'title', 
+                        'color', 
+                        'is_base_project', 
+                        'user_id', 
+                        'tasks'
+                        ]
+
+
+def test_create_task():
+    global test_task_id
+    response = client.post(
+                        "/task",
+                        headers={"Authorization": "Bearer "+token},
+                        json={
+                            "title": "string",
+                            "description": "string",
+                            "position": 0,
+                            "priority": 0,
+                            "section": "string",
+                            "datetime_expiration": "2022-12-06T17:28:06.314Z",
+                            "project_id": test_project_id
+                            }
+                        )
+    assert response.status_code == 200
+    json = response.json()
+    for key, value in json.items():
+        assert key in [
+                        "title",
+                        "description",
+                        "position",
+                        "priority",
+                        "section",
+                        "datetime_expiration",
+                        "id",
+                        "is_completed",
+                        "datetime_completion",
+                        "datetime_added",
+                        "project_id"
+                        ]
+    test_task_id = json['id']
+
+
+def test_update_task():
+    response = client.put(
+                        "/task",
+                        headers={"Authorization": "Bearer "+token},
+                        json={
+                            "id": test_task_id,
+                            "title": "string_new"
+                            }
+                        )
+    assert response.status_code == 200
+    json = response.json()
+    for key, value in json.items():
+        assert key in [
+                        "title",
+                        "description",
+                        "position",
+                        "priority",
+                        "section",
+                        "datetime_expiration",
+                        "id",
+                        "is_completed",
+                        "datetime_completion",
+                        "datetime_added",
+                        "project_id"
+                        ]
+
+
+
+def test_update_user():
+    response = client.put(
+                        "/user",
+                        headers={"Authorization": "Bearer " + token},
+                        json={
+                            "username": TEST_NAME + str(random.randint(1, 444)),
+                            "email": str(random.randint(1, 444)) + TEST_EMAIL,
+                            "password": TEST_PASSWORD + str(random.randint(1, 444))
+                            }
+                        )
+    assert response.status_code == 200
